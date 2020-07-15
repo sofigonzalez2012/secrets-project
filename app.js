f199922ea445280476dca71b8afd4440db5ad997
Sofía González <sofigonzalez2012@gmail.com>
2020-07-14 22:00:57 -0500

    Initial commit with env vbls

---
+++ app.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+//Schema
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+//Secret to encrypt the DB
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] }); //Encryption is set up (secret and fields to encrypt)
+
+//Model
+const User = mongoose.model("User", userSchema);
+
+
+//Methods
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", function(req, res) {
+
+  const user = new User ({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  user.save(function(err) { //Here the password gets encrypted
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets"); //Only render the secrets page when the user is registred
+    }
+  });
+});
+
+
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne(
+    {email: username},
+    function(err, foundUser) {
+      if (err) {  //Error
+        console.log(err);
+      } else {
+        if (foundUser) { //The user does exist
+          //Here the password gets decrypted
+          if (foundUser.password === password) { //The password matches the user found
+            res.render("secrets");
+          }
+        }
+      }
+    }
+  );
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
